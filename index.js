340f8dd25e33aeaf88d4312a670f683fe9d6f506
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-07-31 05:39:32 +0100

    Done with the assignement

--- index.js
+++ index.js
@@ -16,17 +16,3 @@ const record = [
 
   const winningYear = superbowlWin(record);
   console.log(winningYear);
-
-//   function superbowlWin(array) {
-//     for (const data of array) {
-//       // Use "W" (capital "W") instead of "w"
-//       if (data.result === "W") {
-//         // Use "data.year" instead of "array.year"
-//         return data.year;
-//       }
-//     }
-//   }
-  
-//   const winningYear = superbowlWin(record);
-//   console.log(winningYear); // Output will be the year of the win or undefined if no win is found
-  
